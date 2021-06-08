091d5a2ce398a42a7b992e0449bceb4521b1353f
sheikscorner <85100480+sheikscorner@users.noreply.github.com>
2021-06-08 13:31:43 +0530

    updated final bug fixes

--- environment/main.py
+++ environment/main.py
@@ -119,7 +119,7 @@ async def dna_india(request: Request):
             dict1[str(text_data)].append(article.summary)
         except Exception:
             del dict1[str(text_data)]
-    print(dict1)
+    
     json_compatible_item_data = jsonable_encoder(dict1)
     return templates.TemplateResponse("display.html", {"request":request, "json_data":json_compatible_item_data})
 
